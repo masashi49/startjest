6d90815893dbe9ce32990c66c72458c662558f93
yamadamasashi <shisama94@gmail.com>
2023-05-12 00:35:46 +0900

    2.4.3まで完了57ページ

--- next.test.ts
+++ next.test.ts
@@ -21,11 +21,44 @@ test.each([{ a: 1, b: 2, expected: 3 }])('足し算', (data) => {
   expect(data.a + data.b).toBe(data.expected);
 });
 
-test.each([
+type TestParams = {
+  a: number;
+  b: number;
+  expected: number;
+};
+test.each<TestParams>([
   { a: 1, b: 1, expected: 2 },
   { a: 1, b: 2, expected: 3 },
   { a: 2, b: 1, expected: 3 },
 ])('.add($a, $b)', ({ a, b, expected }) => {
   // 展開して受け取る方が直感的
   expect(a + b).toBe(expected);
+});
+test.each([{ a: 'hello', b: 'world', expected: 'helloworld' }])(
+  '.add($a, $b)',
+  ({ a, b, expected }) => {
+    // 展開して受け取る方が直感的
+    expect(a + b).toBe(expected);
+  }
+);
+
+function tax(amount: number) {
+  if (amount <= 0) return 0;
+  const taxRate = 0.1;
+  const taxAmount = Math.round(amount * taxRate); // round 小数点第一位を四捨五入
+  return taxAmount;
+}
+
+test('消費税10％テスト', () => {
+  expect(tax(100)).toBe(10);
+});
+
+test.each([
+  { a: 100, expected: 10 },
+  { a: 320, expected: 32 },
+  { a: 7654, expected: 765 },
+  { a: 0, expected: 0 },
+  { a: -2435, expected: 0 },
+])('消費税10％テストEach', ({ a, expected }) => {
+  expect(tax(a)).toBe(expected);
 });
